879139d42d5f047a6c4b4aaf05b1bab22d21d9e5
Shreyashj9600 <139689092+Shreyashj9600@users.noreply.github.com>
2025-05-10 08:19:36 +0000

    Named & Aggregate Exports in Node.js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const add = require('./math')
+// const add = require('./math')
+
+// Destracture
+const{ add, sub, mult, div } = require('./math')
 
 console.log(add(5,10))
+console.log(sub(5,10))
+console.log(mult(5,10))
+console.log(div(5,10))
+
+
+// const math = require('./math')
+
+// repatitev
+// console.log(math.add(5,10))
+// console.log(math.sub(5,10))
+// console.log(math.mult(5,10))
+// console.log(math.div(5,10))
+
+
+
+
+// module.exports is a single value or object 
+// when you reassign module.exports (e.g: module.exports = add ) its completely replace watever was previously assigned.
+// if you reassign again (module.exports = mult), the previous value (add) is replaced with mult.
--- math.js
+++ math.js
@@ -2,4 +2,24 @@ const add = (num1, num2) => {
     return num1 + num2
 }
 
-module.exports = add;
+const sub = (num1, num2) => {
+    return num1 - num2
+}
+
+const mult = (num1, num2) => {
+    return num1 * num2
+}
+
+const div = (num1, num2) => {
+    return num1 / num2
+}
+
+// 1
+// module.exports = add;
+
+// 2
+module.exports.add = add
+module.exports.sub = sub
+
+// 3
+module.exports = { add, sub, mult, div }
