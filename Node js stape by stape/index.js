9206cc34a314b861dac1233812c9966400cb3590
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-05-19 14:50:53 +0530

    async programming in node js

--- Node js stape by stape/index.js
+++ Node js stape by stape/index.js
@@ -1,16 +1,8 @@
-const fs = require('fs')
-const path = require('path')
-// fs.writeFileSync('apple.txt', 'this is a fruit');
+let a = 20;
+let b = 0;
 
-const dirPath = path.join(__dirname, 'files')
-console.log(dirPath);
-
-// for (let i = 0; i < 5; i++) {
-//     fs.writeFileSync(dirPath + '/hello' + i + ' .txt', 'testing file and file no is '+ i +'')
-// }
-
-fs.readdir(dirPath,(err, files) => {
-    files.forEach((item)=> {
-        console.log(`file name is ${item}`);
-    })
+setTimeout(() => {
+    b = 30
 })
+
+console.log(a + b);
