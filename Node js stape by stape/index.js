1e91a31d5f154651988a0ee6709e1e856ca5a33f
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-05-19 15:08:07 +0530

    async data handel in node js

--- Node js stape by stape/index.js
+++ Node js stape by stape/index.js
@@ -1,8 +1,13 @@
 let a = 20;
 let b = 0;
-
-setTimeout(() => {
-    b = 30
+ 
+let waitingData = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(30)
+    }, 2000)
 })
 
-console.log(a + b);
+waitingData.then((data) => {
+    b = data
+    console.log(a + b);
+})
