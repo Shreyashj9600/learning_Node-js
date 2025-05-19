971556bf2fae4cdd2b99aa83feb937a47a450b89
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-05-19 11:06:37 +0530

    input from cmd

--- Node js stape by stape/index.js
+++ Node js stape by stape/index.js
@@ -1,5 +1,19 @@
- const colors = require('colors')
+// console.log('shreyash');
 
- console.log('hello' .red);
- console.log('hello' .green);
- 
+// console.log(process);
+// console.log(process.argv);
+// console.log(process.argv[2]);
+
+const fs = require('fs')
+
+const input = process.argv;
+
+// fs.writeFileSync(input[2], input[3]) // node .\index.js apple.txt 'file create from cmd'
+
+if (input[2] === 'add') {
+    fs.writeFileSync(input[3], input[4])
+} else if (input[2] === 'remove') {
+    fs.unlinkSync(input[3])
+} else {
+    console.log('invalid outpu');
+}
