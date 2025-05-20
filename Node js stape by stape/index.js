6599a16ab3767d67d9a8288b348ac3c1d8ef1046
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-05-20 17:38:41 +0530

    add html page

--- Node js stape by stape/index.js
+++ Node js stape by stape/index.js
@@ -1,44 +1,10 @@
 const express = require('express')
+const path = require('path')
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('data send from browser', req.query.name);
-    res.send(`
-        <h1>welcome, this is home page</h1>
-        <a href='/about'>go to about page </a>
-        `);
-    res.end()
-})
+const publicPath = path.join(__dirname, 'public')
 
-app.get('/about', (req, res) => {
-    res.send(`
-            <input type='text' placeholder='enter a name' value='${req.query.name}'/>
-            <button>click me</button>
-            <a href='/'>go to home page<a/>
-        `)
-        res.end()
-})
-
-app.get('/help', (req, res) => {
-    res.send([
-        {
-            name: 'shreyash',
-            email: 'shreyash@gmail.com'
-        },
-        {
-            name: 'yash',
-            email: 'yash@gmail.com'
-        },
-        {
-            name: 'shreyash',
-            email: 'shreyash@gmail.com'
-        },
-        {
-            name: 'yash',
-            email: 'yash@gmail.com'
-        }]
-    )
-})
+app.use(express.static(publicPath))
 
 app.listen(3000)
