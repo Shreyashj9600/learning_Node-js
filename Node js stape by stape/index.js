16ab7070bea169ab19e42548c429d6cd9186c78d
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-05-20 16:41:38 +0530

    render html and json

--- Node js stape by stape/index.js
+++ Node js stape by stape/index.js
@@ -1,14 +1,44 @@
- const express = require('express')
+const express = require('express')
 
- const app = express();
+const app = express();
 
- app.get('/', (req,res) => {
-    res.send('welcome, this is home page');
+app.get('/', (req, res) => {
+    console.log('data send from browser', req.query.name);
+    res.send(`
+        <h1>welcome, this is home page</h1>
+        <a href='/about'>go to about page </a>
+        `);
     res.end()
- })
+})
 
- app.get('/about',(req,res) => {
-    res.send('hello this is about page ')
- })
+app.get('/about', (req, res) => {
+    res.send(`
+            <input type='text' placeholder='enter a name' value='${req.query.name}'/>
+            <button>click me</button>
+            <a href='/'>go to home page<a/>
+        `)
+        res.end()
+})
 
- app.listen(3000)
+app.get('/help', (req, res) => {
+    res.send([
+        {
+            name: 'shreyash',
+            email: 'shreyash@gmail.com'
+        },
+        {
+            name: 'yash',
+            email: 'yash@gmail.com'
+        },
+        {
+            name: 'shreyash',
+            email: 'shreyash@gmail.com'
+        },
+        {
+            name: 'yash',
+            email: 'yash@gmail.com'
+        }]
+    )
+})
+
+app.listen(3000)
