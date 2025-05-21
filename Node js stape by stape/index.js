227f01eebee4f494aa54b6d919aa78a5b7cec0ee
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-05-21 15:33:52 +0530

    how to remove extenstions

--- Node js stape by stape/index.js
+++ Node js stape by stape/index.js
@@ -4,7 +4,22 @@ const path = require('path')
 const app = express();
 
 const publicPath = path.join(__dirname, 'public')
+// app.use(express.static(publicPath))
 
-app.use(express.static(publicPath))
+app.get('', (_, res) => {
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about', (_, res) => {
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/home', (_, res) => {
+    res.sendFile(`${publicPath}/home.html`)
+})
+
+app.get("*", (_, res) => {
+    res.sendFile(`${publicPath}\error.html`)
+})
 
 app.listen(3000)
