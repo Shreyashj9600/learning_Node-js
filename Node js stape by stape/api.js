862a3658c46a2ac9c2e94319959a7577e95d6465
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-06-03 16:18:17 +0530

    put api update data in mongo db 2

--- Node js stape by stape/api.js
+++ Node js stape by stape/api.js
@@ -2,11 +2,20 @@ const express = require('express')
 const dbConnect = require('./mongodb')
 const app = express()
 
-app.get('/', async(req,res) => {
+app.use(express.json())
+
+app.get('/', async (req, res) => {
     let data = await dbConnect();
     data = await data.find().toArray();
     console.log(data);
     res.send(data)
+})
+
+app.post('/', async (req, res) => {
+    let data = await dbConnect();
+    let result = await data.insertOne(req.body)    
+    res.send(result)
+
 })
 
 app.listen(3000)
