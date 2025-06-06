dbb1a52893472db22635eb5d54e4138d0d3e8e85
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-06-06 17:13:52 +0530

    get put delete post method in node js

--- Node js stape by stape/api.js
+++ Node js stape by stape/api.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dbConnect = require('./mongodb')
 const app = express()
+const mongoDB = require('mongodb')
 
 app.use(express.json())
 
@@ -13,9 +14,27 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
     let data = await dbConnect();
-    let result = await data.insertOne(req.body)    
+    let result = await data.insertOne(req.body)
     res.send(result)
 
+})
+
+app.put('/', async (req, res) => {
+    console.log(req.body);
+    let data = await dbConnect();
+    let result = data.updateOne(
+        { name: "i phone 15" }, // {name:req.params.name}
+        { $set: { price: 55 } }      // {$set:req.body}
+    )
+    res.send({ result: "updated" })
+})
+
+app.delete("/:id", async (req, res) => {
+    console.log(req.params.id);
+    const data = await dbConnect();
+    const result = await  data.deleteOne({ _id: new mongoDB.ObjectId(req.params.id) })
+    
+    res.send(result)
 })
 
 app.listen(3000)
